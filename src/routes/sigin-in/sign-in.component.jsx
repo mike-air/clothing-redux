c71d84a24dce40d87de765711d1f571ea84ca2da
Michael Anderson <mikeandy3310@gmail.com>
2023-01-21 15:48:18 +0000

    added sign in with redirect and deleted it just for practice purpose

--- src/routes/sigin-in/sign-in.component.jsx
+++ src/routes/sigin-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect
 } from "../../utils/firebase/firebase.utils";
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -8,10 +9,20 @@ const SignIn = () => {
    const useDocRef = await  createUserDocumentFromAuth(user);
     console.log(user);
   };
+
+
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect();
+    console.log(user);
+  };
+
   return (
     <div>
       SignIn
       <button onClick={logGoogleUser}>Sign in with Google Pop up</button>
+      <button onClick={logGoogleRedirectUser}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
